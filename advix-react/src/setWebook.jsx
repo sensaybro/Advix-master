b419ee6c15dfac35da12b3d8be005fa438225a86
AmetDev <ametsejdaliev@gmail.com>
2024-04-24 22:09:58 +0300

    fixed

---
+++ advix-react/src/setWebook.jsx
@@ -0,0 +1,21 @@
+async function setWebhook() {
+	try {
+		const response = await fetch(
+			'https://bb35-103-125-235-24.ngrok-free.app/setWebhook',
+			{
+				method: 'POST',
+				headers: {
+					'Content-Type': 'application/json',
+				},
+				body: JSON.stringify({
+					url: 'https://bb35-103-125-235-24.ngrok-free.app/webhook',
+				}),
+			}
+		)
+
+		const data = await response.json()
+		console.log(data)
+	} catch (error) {
+		console.error('Ошибка при установке вебхука:', error)
+	}
+}
