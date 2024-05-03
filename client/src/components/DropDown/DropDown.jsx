c87801b8d6f45c3a44fcdc6028b63da9a048861e
AmetDev <ametsejdaliev@gmail.com>
2024-05-03 15:59:44 +0300

    add-dropdown

--- client/src/components/DropDown/DropDown.jsx
+++ client/src/components/DropDown/DropDown.jsx
@@ -1,36 +1,56 @@
-import { useState } from 'react'
-
+import { useRef, useState } from 'react'
+import style from './DropDown.module.scss'
 const DropDown = ({ default_arg, args }) => {
 	const [clicked, setClicked] = useState(false)
 	const [clickedHidden, setClickedHidden] = useState(false)
 
+	// Create a ref for each li element
+	const refsLi = useRef([])
+
 	const [selectedElement, setSelectedElement] = useState('')
-	const handleClickedElement = element => {
+	const [counter, setCounter] = useState(0)
+	const handleClickedElement = (element, index) => {
 		setClicked(!clicked)
+
 		setSelectedElement(element)
+		if (selectedElement === element && counter === 1) {
+			refsLi.current.forEach(el => (el.style.backgroundColor = 'transparent'))
+			refsLi.current.forEach(el => (el.style.color = '#99a8b2'))
+			setCounter(0)
+			return
+		}
+		setCounter(1)
+		refsLi.current.forEach(el => (el.style.backgroundColor = 'transparent'))
+		refsLi.current.forEach(el => (el.style.color = '#99a8b2'))
+
+		refsLi.current[index].style.backgroundColor = '#00a5e9'
+
+		refsLi.current[index].style.color = '#fff'
 	}
 	const handleClickedHidden = () => {
 		setClickedHidden(!clickedHidden)
 	}
 	return (
-		<div>
+		<div className={style.wrapperDropDown}>
 			<button onClick={() => handleClickedHidden()}>
 				{!clicked ? default_arg : selectedElement}
 			</button>
-			{handleClickedHidden &&
-				args.map(element => {
-					return (
-						<ul>
+			{clickedHidden && (
+				<ul>
+					{args.map((element, index) => {
+						return (
 							<li
+								ref={el => (refsLi.current[index] = el)}
 								onClick={() => {
-									handleClickedElement(element)
+									handleClickedElement(element, index)
 								}}
 							>
 								{element}
 							</li>
-						</ul>
-					)
-				})}
+						)
+					})}
+				</ul>
+			)}
 		</div>
 	)
 }
